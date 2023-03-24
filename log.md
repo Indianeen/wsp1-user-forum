fb16771d0906b24cec21e6b737e98d90fce297fe
Indianeen <axel.lundberg@elev.ga.ntig.se>
2023-03-24 11:11:04 +0100

    Log Lagt till forum.js & user.js login i index.js (lösenordcheck)

--- log.md
+++ log.md
@@ -1,10 +1,18 @@
+## 24/03-23
+Vad gjorde du sist?
+    
+    Clonade arbetet och lade till .env samt justerade gitignore
 
+Vad ska du göra idag?
+    
+    Skriva kod för login- börjar med bekräftning av lösenord och anv.namn 
 
+Vad hindrar dig?
 
+    För dagens arbete: väldigt lite förutom personliga färdigheter / brist på färdigheter
 
-Vad gjorde jag sist?
+Vad har du gjort idag?
 
-Vad ska jag göra idag?
+    Jag har skrivit login och råkat skriva koden i fel arbete som jag fixat
 
-Är det något som hindrar mina framsteg? Vad?
-
+## xx/xx-23
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,53 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcrypt')
+
 /* GET home page. */
 router.get('/', function (req, res) {
     return res.json({ msg: 'Hello World' });
 });
 
 module.exports = router;
+
+router.get('/login', function (req, res, next){
+    
+});
+
+//Innan login anv.namn & pw check
+
+router.post('/login', async function (req, res, next) {
+    const { username, password } = req.body;
+    const errors = [];
+
+    // Kollar om något fält är tomt
+    if (username === "") {
+        errors.push("Username is required")
+        return res.json(errors)
+    } else if (password === "") {
+        errors.push("Password is required")
+        return res.json(errors)
+    }
+
+    // Hämta informationen på användaren
+    const [users] = await promisePool.query("SELECT * FROM xxxx WHERE name=?", username);
+
+    // om användaren finns eller inte
+    if (users.length > 0) {
+
+        bcrypt.compare(password, users[0].password, function (err, result) {
+            if (result) {
+                req.session.userId = username;
+                req.session.LoggedIn = true;
+                return res.redirect('/profile');
+            } else {
+                errors.push("Invalid username or password");
+                return res.json(errors);
+            }
+        });
+    } else {
+        errors.push("Wrong credentials");
+        return res.json(errors);
+    }
+});
+
+//Efter login anv.namn & pw check
